c3881b4121136bb1ee90d598b681493db80eb188
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-05-25 21:28:03 -0300

    feat(app/app.py): new functions.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,3 @@
-from ast import Str
-import string
 from flask import Flask
 import requests
 import json
@@ -11,63 +9,122 @@ GITURL = "https://api.github.com"
 PAT_USER = ""
 HEADER = {"Authorization": f"Bearer {PAT_USER}",
           "Content-Type": "application/vnd.github.v3+json"}
-ORGANIZATION = "madeiramadeirabr"
+# ORGANIZATION = "madeiramadeirabr"
+ORGANIZATION = "Pasto-1"
 USER = "user"
+DEPLOYS_TEAM = "deploys"
 
 
 def run(event, context):
     repository_title = event["repository_title"]
     private = event["private"]
-    #team_owner = event["team"]
+    team_owner = event["team_owner"]
     
     if check_repository(repository_title).status_code == 404:
-        create_repository(repository_title, private)
+        if get_team_by_name(team_owner).status_code == 200:
+            create_repository(repository_title, team_owner, private)
+            add_team(repository_title, team_owner, "push")
+            add_team(repository_title, f"{team_owner}-admin", "admin")
+            add_team(repository_title, DEPLOYS_TEAM, "admin")
+            
+            return {
+                "status": 200,
+                "message": f'Repository {repository_title} created Successfully'            
+                }
         
-        return {
-            "status": "200",
-            "message": "Repository created Successfully"            
+        else:
+            return {
+                "status": 404,
+                "message": f'Failure to find team {team_owner} at {ORGANIZATION}.'
             }
     
     else:
         return {
-            "status": "404",
-            "message": "Repository already exists"
+            "status": 404,
+            "message": f'Repository {repository_title} already exists at {ORGANIZATION}.'
         }
 
 
-def get_teams_by_name(team_name):
-    team = requests.get(GITURL + "/orgs/" + ORGANIZATION + "/teams/" + team_name, headers=HEADER)
+## GET team_id at Org
+def get_team_by_id(team_name):
+    find_team = requests.get(f'{GITURL}/orgs/{ORGANIZATION}/teams/{team_name}', headers=HEADER)
+   
+    team = json.loads(find_team.text)
     
+    team_id = team["id"]
+    
+    return team_id
+
+
+## GET team_name at Org
+def get_team_by_name(team_name):
+    team = requests.get(f'{GITURL}/orgs/{ORGANIZATION}/teams/{team_name}', headers=HEADER)
     return team
 
-
-def create_repository(repository_name, private):
+## Create Repository
+def create_repository(repository_name, team_owner, private): 
+    team_format = format_string(team_owner)
+    team = get_team_by_id(team_format)
     
     payload = {
         "name": repository_name,
         "description": "Repository create with Lambda",
+        "team_id": team,
         "private": private
     }
     
     data = json.dumps(payload)
     
-    new_repository = requests.post(f'{GITURL}/{USER}/repos', headers=HEADER, data=data)
+    new_repository = requests.post(f'{GITURL}/orgs/{ORGANIZATION}/repos', headers=HEADER, data=data)
     
-    if new_repository.status_code == 404:
+    if new_repository.status_code != 201:
+        error_data = new_repository.text
+        error_data = json.loads(error_data)
+        
         return {
             "status": new_repository.status_code,
-            "message": "Error at proccess 'create_repository'."            
+            "message": f"{error_data['message']}",
+            "documentation_url": f"{error_data['documentation_url']}."          
         }
     
     return new_repository
 
 
-def check_repository(repository_name):
-    repository = requests.get(GITURL + "/repos/" + USER + "/" + repository_name, headers=HEADER)
+## Verify respository_name already exists at Org
+def check_repository(repository_name): 
+    repository_format = format_string(repository_name)
+    repository = requests.get(f'{GITURL}/repos/{ORGANIZATION}/{repository_format}', headers=HEADER)
     
     return repository
 
 
+## Add Team an Repository
+def add_team(repository_name, team_slug, permission_level):
+    repository = format_string(repository_name)
+    team = format_string(team_slug)
+    
+    payload = {
+        "permission": permission_level
+    }
+    
+    data = json.loads(payload)
+    
+    result = requests.put(f'{GITURL}/orgs/{ORGANIZATION}/teams/{team}/{ORGANIZATION}/{repository}', headers=HEADER, data=data)
+    
+    if result.status_code != 204:
+        error_data = result.text
+        error_data = json.loads(error_data)
+        
+        return {
+            "status": result.status_code,
+            "message": f"{error_data['message']}",
+            "documentation_url": f"{error_data['documentation_url']}."          
+        }
+    
+    return result
+
+
+## Create README.md
 def create_readme(title, description, bussiness_context, requirements=list, integration=None, owner=None,):
     
     owner_link = owner.lower()
@@ -101,3 +158,9 @@ def create_readme(title, description, bussiness_context, requirements=list, inte
     readme = markdow.create_md_file()
     
     return readme
+
+def format_string(parameter):
+    lower_string = parameter.replace(" ", "-")
+    result = lower_string.lower()
+    
+    return result 
