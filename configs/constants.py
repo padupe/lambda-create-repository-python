1fcb3efb0bbe7848332ef9274a930c14e865e497
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-06-01 09:48:54 -0300

    fix(configs): remove value at PAT_USER constant.

--- configs/constants.py
+++ configs/constants.py
@@ -1,7 +1,7 @@
 from os import environ
 
 GITHUB_URL = "https://api.github.com"
-PAT_USER = "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"
+PAT_USER = ""
 HEADER = {"Authorization": f"Bearer {PAT_USER}",
           "Content-Type": "application/vnd.github.v3+json"}
 # ORGANIZATION = "madeiramadeirabr"
