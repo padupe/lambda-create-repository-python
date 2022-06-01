cde098f3130afb8a60541de385c3a465eb344b0c
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-06-01 12:09:06 -0300

    fix(configs/constants.py): remove comment.

--- configs/constants.py
+++ configs/constants.py
@@ -4,13 +4,10 @@ GITHUB_URL = "https://api.github.com"
 PAT_USER = ""
 HEADER = {"Authorization": f"Bearer {PAT_USER}",
           "Content-Type": "application/vnd.github.v3+json"}
-# ORGANIZATION = "madeiramadeirabr"
 ORGANIZATION = "Pasto-1"
-USER = "user"
 DEPLOYS_TEAM = "deploys"
 
 """
-
 TODO - Create Secrets Manager on AWS:
 
 PAT_USER = environ["PAT_USER"]
